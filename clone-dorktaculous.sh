cda5f2cf3927ff7c902028add70d9db9b89f16a9
gary kissell <garykissell@yahoo.com>
2025-01-14 21:51:58 -0600

    Update clone-dorktaculous.sh

--- clone-dorktaculous.sh
+++ clone-dorktaculous.sh
@@ -1,13 +1,7 @@
 #!/bin/bash 
-
+bash
 mkdir dorkbox
 mkdir dorkbox/templates
+cd dorkbox
+git clone https://github.com/hackslikeus/dorktaculous.git
 
--s https://api.github.com/users/hackslikeus/repos/dorktaculous
-
-curl -s https://api.github.com/users/hackslikeus/repos/dorktaculous | grep ssh_url | cut -d'"' -f 4 
-
-for repo in $repos
-do
-  git clone $repo
-done 
