15b6134f85d3c1d8a32dde28272bf14cd106d8f7
gary kissell <garykissell@yahoo.com>
2025-01-16 11:02:44 -0600

    Update clone-dorktaculous.sh
    add a few moves at the end

--- clone-dorktaculous.sh
+++ clone-dorktaculous.sh
@@ -4,4 +4,7 @@ mkdir dorkbox
 mkdir dorkbox/templates
 cd dorkbox
 git clone https://github.com/hackslikeus/dorktaculous.git
-
+mv index.html /var/www/index.nginx-debian.html
+mv testing_index1219.html /var/www/testingindex.html
+mv demo_iframe.html /var/www/demo_iframe.html
+done
